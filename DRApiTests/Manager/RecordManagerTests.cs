d07fd9175fc928d4473f5468987556a931f94c5c
SorenHersomNielsen <hersom@ellekrattet.dk>
2021-04-07 11:15:30 +0200

    Test

---
+++ DRApiTests/Manager/RecordManagerTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DRApi.Manager;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DRApi.Model;
+
+namespace DRApi.Manager.Tests
+{
+    [TestClass()]
+    public class RecordManagerTests
+    {
+        Record record;
+
+        [TestInitialize]
+        public void StartTest()
+        {
+            record = new Record();
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            var list = RecordManager.RecordList.Count;
+            Assert.AreEqual(list, RecordManager.RecordList.Count);
+        }
+
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByTitleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByArtistTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
