47757d58dc22714182ea1695a292d9614d235287
SorenHersomNielsen <hersom@ellekrattet.dk>
2021-04-07 10:39:41 +0200

    Add project files.

---
+++ DRApi/Manager/RecordManager.cs
@@ -0,0 +1,22 @@
+﻿using DRApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DRApi.Manager
+{
+    public class RecordManager
+    {
+        public static List<Record> RecordList = new List<Record>()
+        {
+            new Record{Title = "Øde Ø", Artist ="Rasmus Seebach", Duration = 180, YearOfPublication = 2013},
+            new Record{ Title = "Pas På Den Knaldrøde Gummibåd", Artist = "Birthe Kjær",Duration = 180, YearOfPublication = 1971}
+        };
+
+        public List<Record> GetAll()
+        {
+            return new List<Record>(RecordList);
+        }
+    }
+}
---
+++ DRApi/Model/Record.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DRApi.Model
+{
+    public class Record
+    {
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string artist;
+
+        public string Artist
+        {
+            get { return artist; }
+            set { artist = value; }
+        }
+
+        private int duration;
+
+        public int Duration
+        {
+            get { return duration; }
+            set { duration = value; }
+        }
+
+        private int yearOfPublication;
+
+        public int YearOfPublication
+        {
+            get { return yearOfPublication; }
+            set { yearOfPublication = value; }
+        }
+    }
+}
