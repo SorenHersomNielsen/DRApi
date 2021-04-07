4a19a26c0d5ec2fda6077ab268695b106c29dc0d
MathiasMeisner <62882868+MathiasMeisner@users.noreply.github.com>
2021-04-07 11:45:49 +0200

    Update
    test

--- DRApiTests1/Manager/RecordManagerTests.cs
+++ DRApiTests1/Manager/RecordManagerTests.cs
@@ -1,6 +1,39 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DRApi.Manager;
+using System;
 using System.Collections.Generic;
 using System.Text;
+
+namespace DRApi.Manager.Tests
+{
+    [TestClass()]
+    public class RecordManagerTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByTitleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByArtistTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
 
 namespace DRApiTests1.Manager
 {
